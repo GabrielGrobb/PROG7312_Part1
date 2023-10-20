5ee87015c1f38377a0c4843ea00716d23defe449
Gabriel Grobbelaar <gabrielgrobbelaar1703@gmail.com>
2023-10-20 14:47:39 +0200

    Added a background image for Identify Areas and designed the identify areas user control.

--- PROG7312_Part1/IdentifyAreasUserControl.Designer.cs
+++ PROG7312_Part1/IdentifyAreasUserControl.Designer.cs
@@ -28,10 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.leftShelf3 = new System.Windows.Forms.Panel();
+            this.leftShelf2 = new System.Windows.Forms.Panel();
+            this.leftShelf1 = new System.Windows.Forms.Panel();
+            this.leftShelf0 = new System.Windows.Forms.Panel();
+            this.rightShelf0 = new System.Windows.Forms.Panel();
+            this.rightShelf1 = new System.Windows.Forms.Panel();
+            this.rightShelf2 = new System.Windows.Forms.Panel();
+            this.rightShelf3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHelp.Location = new System.Drawing.Point(830, 73);
+            this.btnHelp.Margin = new System.Windows.Forms.Padding(2);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(138, 38);
+            this.btnHelp.TabIndex = 38;
+            this.btnHelp.Text = "Help?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.BackColor = System.Drawing.Color.Transparent;
+            this.btnMainMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMainMenu.Location = new System.Drawing.Point(830, 14);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(138, 38);
+            this.btnMainMenu.TabIndex = 37;
+            this.btnMainMenu.Text = "Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = false;
+            // 
+            // btnResults
+            // 
+            this.btnResults.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResults.Location = new System.Drawing.Point(15, 260);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(138, 38);
+            this.btnResults.TabIndex = 36;
+            this.btnResults.Text = "View Results";
+            this.btnResults.UseVisualStyleBackColor = true;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.Location = new System.Drawing.Point(15, 200);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(138, 38);
+            this.btnRestart.TabIndex = 35;
+            this.btnRestart.Text = "Restart?";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.Location = new System.Drawing.Point(15, 14);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(138, 38);
+            this.btnStart.TabIndex = 34;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.ForeColor = System.Drawing.Color.Lime;
+            this.lblTimer.Location = new System.Drawing.Point(209, 14);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(257, 38);
+            this.lblTimer.TabIndex = 33;
+            this.lblTimer.Text = "label1";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Location = new System.Drawing.Point(15, 137);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(138, 38);
+            this.btnReset.TabIndex = 32;
+            this.btnReset.Text = "Reset Columns";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // leftShelf3
+            // 
+            this.leftShelf3.BackColor = System.Drawing.Color.Silver;
+            this.leftShelf3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftShelf3.Location = new System.Drawing.Point(365, 324);
+            this.leftShelf3.Name = "leftShelf3";
+            this.leftShelf3.Size = new System.Drawing.Size(101, 38);
+            this.leftShelf3.TabIndex = 42;
+            // 
+            // leftShelf2
+            // 
+            this.leftShelf2.BackColor = System.Drawing.Color.Silver;
+            this.leftShelf2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftShelf2.Location = new System.Drawing.Point(365, 264);
+            this.leftShelf2.Name = "leftShelf2";
+            this.leftShelf2.Size = new System.Drawing.Size(101, 38);
+            this.leftShelf2.TabIndex = 41;
+            // 
+            // leftShelf1
+            // 
+            this.leftShelf1.BackColor = System.Drawing.Color.Silver;
+            this.leftShelf1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftShelf1.Location = new System.Drawing.Point(365, 201);
+            this.leftShelf1.Name = "leftShelf1";
+            this.leftShelf1.Size = new System.Drawing.Size(101, 38);
+            this.leftShelf1.TabIndex = 40;
+            // 
+            // leftShelf0
+            // 
+            this.leftShelf0.BackColor = System.Drawing.Color.Silver;
+            this.leftShelf0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftShelf0.Location = new System.Drawing.Point(365, 137);
+            this.leftShelf0.Name = "leftShelf0";
+            this.leftShelf0.Size = new System.Drawing.Size(101, 38);
+            this.leftShelf0.TabIndex = 39;
+            // 
+            // rightShelf0
+            // 
+            this.rightShelf0.BackColor = System.Drawing.Color.Silver;
+            this.rightShelf0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightShelf0.Location = new System.Drawing.Point(539, 137);
+            this.rightShelf0.Name = "rightShelf0";
+            this.rightShelf0.Size = new System.Drawing.Size(208, 38);
+            this.rightShelf0.TabIndex = 40;
+            // 
+            // rightShelf1
+            // 
+            this.rightShelf1.BackColor = System.Drawing.Color.Silver;
+            this.rightShelf1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightShelf1.Location = new System.Drawing.Point(539, 201);
+            this.rightShelf1.Name = "rightShelf1";
+            this.rightShelf1.Size = new System.Drawing.Size(208, 38);
+            this.rightShelf1.TabIndex = 41;
+            // 
+            // rightShelf2
+            // 
+            this.rightShelf2.BackColor = System.Drawing.Color.Silver;
+            this.rightShelf2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightShelf2.Location = new System.Drawing.Point(539, 264);
+            this.rightShelf2.Name = "rightShelf2";
+            this.rightShelf2.Size = new System.Drawing.Size(208, 38);
+            this.rightShelf2.TabIndex = 41;
+            // 
+            // rightShelf3
+            // 
+            this.rightShelf3.BackColor = System.Drawing.Color.Silver;
+            this.rightShelf3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightShelf3.Location = new System.Drawing.Point(539, 324);
+            this.rightShelf3.Name = "rightShelf3";
+            this.rightShelf3.Size = new System.Drawing.Size(208, 38);
+            this.rightShelf3.TabIndex = 41;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel1.Location = new System.Drawing.Point(258, 456);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(208, 38);
+            this.panel1.TabIndex = 41;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel2.Location = new System.Drawing.Point(539, 456);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(208, 38);
+            this.panel2.TabIndex = 41;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Silver;
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel3.Location = new System.Drawing.Point(539, 511);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(208, 38);
+            this.panel3.TabIndex = 41;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Silver;
+            this.panel4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel4.Location = new System.Drawing.Point(258, 511);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(208, 38);
+            this.panel4.TabIndex = 41;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Silver;
+            this.panel5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel5.Location = new System.Drawing.Point(258, 564);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(208, 38);
+            this.panel5.TabIndex = 41;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Silver;
+            this.panel6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel6.Location = new System.Drawing.Point(539, 564);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(208, 38);
+            this.panel6.TabIndex = 41;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Silver;
+            this.panel7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panel7.Location = new System.Drawing.Point(409, 627);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(208, 38);
+            this.panel7.TabIndex = 41;
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPause.Location = new System.Drawing.Point(15, 73);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(138, 38);
+            this.btnPause.TabIndex = 43;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(258, 391);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(489, 38);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Descriptions";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // IdentifyAreasUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PROG7312_Part1.Properties.Resources.part2background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rightShelf3);
+            this.Controls.Add(this.rightShelf2);
+            this.Controls.Add(this.rightShelf1);
+            this.Controls.Add(this.rightShelf0);
+            this.Controls.Add(this.leftShelf3);
+            this.Controls.Add(this.leftShelf2);
+            this.Controls.Add(this.leftShelf1);
+            this.Controls.Add(this.leftShelf0);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.btnResults);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnReset);
+            this.DoubleBuffered = true;
+            this.Name = "IdentifyAreasUserControl";
+            this.Size = new System.Drawing.Size(1000, 686);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Panel leftShelf3;
+        private System.Windows.Forms.Panel leftShelf2;
+        private System.Windows.Forms.Panel leftShelf1;
+        private System.Windows.Forms.Panel leftShelf0;
+        private System.Windows.Forms.Panel rightShelf0;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel rightShelf1;
+        private System.Windows.Forms.Panel rightShelf2;
+        private System.Windows.Forms.Panel rightShelf3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label label1;
     }
 }
