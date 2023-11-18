b3728d516774e781f2162681b35a6c51abd5cd08
Gabriel Grobbelaar <gabrielgrobbelaar1703@gmail.com>
2023-11-18 10:57:36 +0200

    Added Calling numbers help form

---
+++ PROG7312_Part1/CNHelpForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PROG7312_Part1
+{
+    partial class CNHelpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CNHelpForm));
+            this.lblHelp = new System.Windows.Forms.Label();
+            this.pBoxHelp = new System.Windows.Forms.PictureBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxHelp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHelp
+            // 
+            resources.ApplyResources(this.lblHelp, "lblHelp");
+            this.lblHelp.Name = "lblHelp";
+            // 
+            // pBoxHelp
+            // 
+            resources.ApplyResources(this.pBoxHelp, "pBoxHelp");
+            this.pBoxHelp.Name = "pBoxHelp";
+            this.pBoxHelp.TabStop = false;
+            // 
+            // btnNext
+            // 
+            resources.ApplyResources(this.btnNext, "btnNext");
+            this.btnNext.Name = "btnNext";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnBack
+            // 
+            resources.ApplyResources(this.btnBack, "btnBack");
+            this.btnBack.Name = "btnBack";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // CNHelpForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblHelp);
+            this.Controls.Add(this.pBoxHelp);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CNHelpForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxHelp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelp;
+        private System.Windows.Forms.PictureBox pBoxHelp;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ PROG7312_Part1/CNHelpForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROG7312_Part1
+{
+    public partial class CNHelpForm : Form
+    {
+        /// <summary>
+        /// Setting the currentImageIndex to zero.
+        /// String array for the images' names.
+        /// </summary>
+        private int currentImageIndex = 0;
+        private string[] imageNames = { "helpImage1", "helpImage2", "helpImage3", "helpImage4", "helpImage5", "helpImage6", "helpImage7", "helpImage8" };
+
+        //------------------------------------------------------------------------------------------//
+
+        /// <summary>
+        /// Displaying the images when the form is opened.
+        /// </summary>
+        public CNHelpForm()
+        {
+            InitializeComponent();
+        }
+
+        //------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Displaying an image in a PictureBox. (blawford, 2014)
+        /// Stretching the image to fit to the appropriate picture box.
+        /// Accessing the images from the resource manager.
+        /// </summary>
+        /// <param name="index"></param>
+        private void DisplayImage(int imageNameIndex)
+        {
+            if (imageNameIndex >= 0 && imageNameIndex < imageNames.Length)
+            {
+                pBoxHelp.SizeMode = PictureBoxSizeMode.StretchImage;
+                pBoxHelp.Image = Properties.Resources.ResourceManager.GetObject(imageNames[imageNameIndex]) as Image;
+
+            }
+        }
+
+        //------------------------------------------------------------------------------------------//
+        /// <summary>
+        ///  A back button to decrement through the array indexes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (currentImageIndex > 0)
+            {
+                currentImageIndex--;
+                DisplayImage(currentImageIndex);
+            }
+        }
+
+        //------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// A next button to increment through the array indexes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentImageIndex < imageNames.Length - 1)
+            {
+                currentImageIndex++;
+                DisplayImage(currentImageIndex);
+            }
+        }
+
+        //------------------------------------------------------------------------------------------//
+    }
+}
+//------------------------------------------EndOfFile-----------------------------------------------//
+
+#region /// REFERENCES - CODE ATTRIBUTION:
+/* 
+ * 
+Aurthor:  blawford
+Webisite: StackOverFlow, 2014/08/27. Load Image from Resources/ResourceManager. [Online]
+Accessed on: 2023/09/26
+URL:https://stackoverflow.com/questions/25520933/load-image-from-resources-resourcemanager
+
+ */
+#endregion
